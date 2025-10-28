1708b179e7bd744d4f72494fe6aad475db7853e4
Chaitanya Galande <chaitanya051203@gmail.com>
2025-10-28 22:37:02 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/CityPolling/CityPollingApplication.java
@@ -0,0 +1,13 @@
+package com.example.CityPolling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityPollingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityPollingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CityPolling/CityPollingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CityPolling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityPollingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
